3ce6244f7a0aa1646842fe2754c1ac965bf3cfbd
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-12 20:50:02 +0545

    Authentication started

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:authentication/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
