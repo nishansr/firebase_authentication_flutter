ba71dd14e201c8e737cb9d5e7d83de61afa51f4f
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-15 08:36:20 +0545

    REEINITIALIZED THE APP

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:authentication/firebase_options.dart';
+import 'package:authentication/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange[900]!),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
